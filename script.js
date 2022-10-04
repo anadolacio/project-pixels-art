d2c90778cf150d5f174282a9eb53f9f6bbf90613
anadolacio <anadolacio@hotmail.com>
2022-10-04 19:53:26 -0300

    Resolução do requisito 5.

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
- window.onload =  function () {
+window.onload = function () {
+  // Button
+  let buttonRandom = document.getElementById("button-random-color");
+  
+  buttonRandom.addEventListener("click", () => {
+  let colorSelected = document.querySelectorAll(".color");
+  let colors = ["black"];
+  for (let i = 1; i < colorSelected.length; i += 1) {
+  let redRandom = Math.floor(Math.random() * 255 + 1);
+  let greenRandom = Math.floor(Math.random() * 255 + 1);
+  let blueRandom = Math.floor(Math.random() * 255 + 1);
+  
+  if (redRandom !== (greenRandom !== blueRandom)) {
+  colorSelected[i].style.backgroundColor = `rgb(${redRandom}, ${greenRandom}, ${blueRandom})`;
+  colors.push(`rgb(${redRandom},${greenRandom},${blueRandom})`);
+  }
+  localStorage.setItem("colorPalette", JSON.stringify(colors));
+  }
+  });
+};
 
-    let buttonRandom = document.querySelector("#button-random-color");
+storageColors();
 
-    buttonRandom.addEventListener("click", function() {
+// Storage
 
-        let colorSelected = document.querySelectorAll(".color");
-
-            for (let i = 1; i < colorSelected.length; i += 1) {
-                let redRandom = Math.floor(Math.random() * 255 + 1);
-                let greenRandom = Math.floor(Math.random() * 255 + 1);
-                let blueRandom = Math.floor(Math.random() * 255 + 1);
-
-                if (redRandom !== (greenRandom !== blueRandom)) {
-                    colorSelected[i].style.backgroundColor = `rgb(${redRandom},${greenRandom},${blueRandom})`;
-                } 
-            }
-   });
+function storageColors() {
+  let colorsSaved = JSON.parse(localStorage.getItem("colorPalette"));
+  let colorSelected = document.getElementsByClassName("color");
+    for (let i = 0; i < colorSelected.length; i += 1) {
+    colorSelected[i].style.backgroundColor = colorsSaved[i];
+    }
 
 };
- 
-
-
